8e0a1d8c75422cb2dbfbe2f59da04694ca4cd57b
TekniqueJoseph <joejacksun@gmail.com>
2023-04-30 13:52:47 -0400

    used query selectors to change styles

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const mainHead = document.querySelector('h1')
 const second = document.querySelector('ul :nth-child(2)');
 const part = document.querySelector('#particles-js')
+
+second.style.color = 'red'
