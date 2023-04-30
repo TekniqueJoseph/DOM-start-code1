3a9eb14ac397f80e28bcacb9a5973b6b200f89f9
TekniqueJoseph <joejacksun@gmail.com>
2023-04-30 14:30:37 -0400

    used console.dir for methods

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const second = document.querySelector('ul :nth-child(2)');
 const part = document.querySelector('#particles-js')
 
 mainHead.textContent = 'Take Your Time!'
+mainHead.className = 'title'
+mainHead.style.color = 'gold'
+mainHead.style.backgroundColor = 'black'
