d3f8f1148fea91658905833f612acf24d3243eca
TekniqueJoseph <joejacksun@gmail.com>
2023-04-30 18:33:57 -0400

    used children method to select list elements

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const mainHead = document.querySelector('h1')
+const ul = document.querySelector('ul')
 const second = document.querySelector('ul :nth-child(2)');
 const third = document.querySelector('li:last-of-type')
 const part = document.querySelector('#particles-js')
 
 mainHead.textContent = 'Take Your Time!'
 mainHead.className = 'title'
-mainHead.style.color = 'gold'
-mainHead.style.backgroundColor = 'black'
+
 
 const input = document.querySelector('input')
 input.setAttribute('value', 'default text')
@@ -14,6 +14,5 @@ input.setAttribute('value', 'default text')
 input.value = input.getAttribute('value', 'enter text')
 const allLis = document.querySelectorAll('li')
 
-const list = document.querySelectorAll('li')
-second.style.color = 'red'
-third.style.color = 'indigo'
+
+ul.children[0].style.color = 'red'
